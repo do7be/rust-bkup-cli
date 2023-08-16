291b0ff7e2a2c088942be607b2718fda6d39f4d3
do7be <bomsap700@gmail.com>
2023-08-16 17:10:40 +0900

    implements bkup command

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use chrono::Local;
+use clap::Parser;
+use std::path::Path;
+
+// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    path: std::path::PathBuf,
+
+    #[arg(short, long)]
+    bak: bool,
+
+    #[arg(short, long)]
+    original: bool,
+
+    #[arg(short, long)]
+    simple: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // 存在チェック
+    if !Path::new(&args.path).exists() {
+        panic!("No such file or directory");
+    }
+
+    let backup_suffix = get_file_suffix(&args);
+
+    let to_path = Path::new(&args.path);
+    let to_path_string = to_path.file_name().unwrap().to_str().unwrap().to_string();
+    let new_filename = to_path_string + "." + &backup_suffix;
+    let to = to_path.with_file_name(new_filename);
+
+    std::fs::copy(&args.path, to).expect("Could not copy file");
+}
+
+fn get_file_suffix(args: &Args) -> String {
+    let dt = Local::now();
+    if args.bak {
+        String::from("bak")
+    } else if args.original {
+        String::from("org")
+    } else if args.simple {
+        dt.format("%Y%m%d").to_string()
+    } else {
+        dt.format("%Y%m%d%H%M%S").to_string()
+    }
+}
